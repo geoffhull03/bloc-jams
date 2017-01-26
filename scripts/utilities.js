a81245c8e4df0091205a37a149f7e9edad3e90ca
Geoff Hull <geoffhull@cacpops-geoffhu.local>
2017-01-25 16:28:29 -0800

    pls be fixed

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,4 @@
 /*fuction forEach(array,callback) {
      for (var i = 0; i < array.length; i++)
        callback(array[i]);
-       I really hope this is fixed now. This is just text so that I have something new to commit 
 */
